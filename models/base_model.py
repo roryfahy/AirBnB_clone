583b1ecb8aa399c788f19986ac7079e7af9efe73
rmf10003 <685@holbertonschool.com>
2019-06-27 16:57:45 +0000

    Finish fourth task, creating simple data model methods

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,52 @@
 #!/usr/bin/python3
-"""module for basemodel"""
+"""Module for basemodel"""
 
 import datetime
 import uuid
 
+
 class BaseModel:
-    """base class for data models"""
-    
+    """Base class for data models
+
+    Attributes:
+        id (str): a random UUID string unique to each instance
+        created_at (datetime.datetime): time stamp when instance was created
+        updated_at (datetime.datetime): time stamp when instance was updated
+
+    """
+
     def __init__(self):
-        """create new instance of BaseModel"""
+        """Create new instance of BaseModel"""
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        """Return the instance's ID, class name, and attributes as a string"""
+
+        return '[{}] ({}) {}'.format(
+            type(self).__name__,
+            self.id,
+            str(self.__dict__)
+        )
+
+    def save(self):
+        """Update the instance's update time"""
+
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """Return this instance's attributes as a dict
+
+        The returned dict also contains a key called "__class__" containing the
+        instance's class name. datetime.datetime objects are converted to
+        strings in ISO 8601 format.
+
+        """
+
+        ret = dict(self.__dict__)
+        ret['__class__'] = type(self).__name__
+        ret['created_at'] = ret['created_at'].isoformat()
+        ret['updated_at'] = ret['updated_at'].isoformat()
+        return ret
