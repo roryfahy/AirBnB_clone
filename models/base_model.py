35b5e925ca12a45eb731d0e0557b6613d43f7c12
rmf10003 <685@holbertonschool.com>
2019-06-27 16:42:02 +0000

    Add initial BaseModel module with testing docstrings

---
+++ models/base_model.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""module for basemodel"""
+
+import datetime
+import uuid
+
+class BaseModel:
+    """base class for data models"""
+    
+    def __init__(self):
+        """create new instance of BaseModel"""
